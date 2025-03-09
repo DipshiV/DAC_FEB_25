d4604b80410c1b75d48aee94b71958d23cde9f2b
Dipshi Verma <130737552+DipshiV@users.noreply.github.com>
2025-03-09 15:51:10 +0530

    Add files via upload

--- OOPJ/Assignment2/CheckNRange.java
+++ OOPJ/Assignment2/CheckNRange.java
@@ -1,6 +1,16 @@
 /*Q9:  Implement a Java program that checks if a number is  within a specific range (20 to 
 50)  without using  if-else  . 
 Hint  : Use  logical AND (  &&  ) in a print statement  . */
-public static CheckNRange{
+import java.util.Scanner;
 
+public class RangeCheckWhile {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); 
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt(); 
+        System.out.println("Is the number within the range (20 to 50)? " + (number >= 20 && number <= 50));
+    }
 }
+
+
+ 
--- OOPJ/Assignment2/DivBy3.java
+++ OOPJ/Assignment2/DivBy3.java
@@ -1,8 +1,28 @@
 /*Q4:  Write a program to find whether a given number is  divisible by 3  without using the 
 modulus (  %  ) or division (  /  ) operators. 
 Hint  : Use  subtraction and bitwise shifts  . */
+
 import java.util.Scanner;
-public class DivBy3{
-	public static void main(String args[]){
-	}
+
+public class DivBy3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+        
+        num = (num < 0) ? -num : num; 
+
+        while (num > 2) {
+            int sum = 0;
+            while (num > 0) {
+                sum += num & 3; 
+                num >>= 2; 
+            }
+            num = sum; 
+			  }
+
+        System.out.println((num == 0) ? "Divisible by 3" : "Not divisible by 3");
+
+    }
 }
